1de614e59ea4691150d1ad98a0fb24726e6abb3b
GabeWong <2275980853@qq.com>
2026-02-13 14:51:14 +0800

    format env test output

--- test.zig
+++ test.zig
@@ -27,7 +27,7 @@ test "env" {
     const std = @import("std");
     const allocator = std.testing.allocator;
     const api_key = std.process.getEnvVarOwned(allocator, "DASHSCOPE_API_KEY") catch {
-        std.debug.print("Environment variable DASHSCOPE_API_KEY is not set.\n", .{});
+        std.debug.print("\nEnvironment variable DASHSCOPE_API_KEY is not set.\n", .{});
         return error.MissingApiKey;
     };
     defer allocator.free(api_key);
